dfca9be62265bd595ca9b8b8518a21a894a83130
Kiersten <kcav1321@gmail.com>
2024-11-22 14:51:35 -0500

    ho

---
+++ PasswordGenerator.java
@@ -0,0 +1,34 @@
+public class PasswordGenerator{
+    private String prefix;
+    private int rand;
+    private int numPass=0;
+    public PasswordGenerator(String p, int r){
+        prefix=p;
+        rand=r;
+    }
+    public PasswordGenerator(int r){
+        rand=r;
+        prefix="a";
+    }
+    public int pwCount(){
+        return numPass;
+    }
+    public String pwGen(){
+        String genPW=prefix+".";
+        numPass+=1;
+        for(int i=rand;i>0;i--){
+            int x=(int)(Math.random()*10);
+            genPW=genPW+x;
+        }
+        return genPW;
+    }
+    public static void main(String[]args){
+        PasswordGenerator password= new PasswordGenerator("hi",8);
+        PasswordGenerator pass2= new PasswordGenerator(10);
+        System.out.println(password.pwGen());
+        System.out.println(pass2.pwGen());
+        System.out.println(password.pwCount());
+        System.out.println(pass2.pwCount());
+    }
+
+}
